2372327fcb2d07cc4ab976e3d0bb5fa3870d43ca
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-12-23 09:28:34 -0300

    Cleaner average stay time service.

--- app/services/average_stay_time_service.rb
+++ app/services/average_stay_time_service.rb
@@ -1,28 +1,41 @@
 class AverageStayTimeService < TableBaseService
-  DEFAULT_STAY_TIME = 3600
+  DEFAULT_STAY_TIME = 1.hour
+  MAX_STAY_TIME = 2.5.hours
 
   def call
     return DEFAULT_STAY_TIME if audits.empty?
 
-    if audits.last.to != Table::STATUS_FREE
-      reservation_id = audits.last.reservation_id
-      @audits = audits.where.not(reservation_id: reservation_id)
-    end
+    completed_ids = complete_reservation_ids
+    return DEFAULT_STAY_TIME if completed_ids.empty?
 
-    reservation_ids = audits.pluck(:reservation_id).uniq
-    return DEFAULT_STAY_TIME if reservation_ids.empty?
-
-    reservations_duration = reservation_ids.inject(0) do |memo, reservation_id|
+    reservations_duration = completed_ids.inject(0) do |memo, reservation_id|
       @audits = audits.where(reservation_id: reservation_id)
-      memo + (audits.last.created_at - audits.first.created_at)
+      table_duration = audits.last.created_at - audits.first.created_at
+
+      if table_duration > MAX_STAY_TIME
+        memo + MAX_STAY_TIME
+      else
+        memo + table_duration
+      end
     end
 
-    average_stay_time = reservations_duration / reservation_ids.count
+    average_stay_time = reservations_duration / completed_ids.count
 
     if average_stay_time > 0
       average_stay_time.to_i
     else
       0
     end
+  end
+
+  private
+
+  def complete_reservation_ids
+    if audits.last.to != Table::STATUS_FREE
+      reservation_id = audits.last.reservation_id
+      @audits = audits.where.not(reservation_id: reservation_id)
+    end
+
+    audits.pluck(:reservation_id).uniq
   end
 end
